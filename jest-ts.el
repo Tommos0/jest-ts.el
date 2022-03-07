d680b26a74f5ad1a082252512796b59513b61109
Tom Klaver <tomklav@gmail.com>
2022-03-07 10:51:05 +0100

    Update

--- jest-ts.el
+++ jest-ts.el
@@ -1,14 +1,26 @@
+;;; jest-ts.el --- Find and run jest tests -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Tom Klaver
+;;
+;; Author: Tom Klaver <https://github.com/tommos0>
+;; Maintainer: Tom Klaver <tomklav@gmail.com>
+;; Created: februari 15, 2022
+;; Modified: februari 15, 2022
+;; Version: 0.0.1
+;; Keywords: tools languages jest test typescript javascript
+;; Homepage: https://github.com/tommos0/jest-ts.el
+;; Package-Requires: ((emacs "25.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
 (require 'tree-sitter)
 (require 'tree-sitter-langs)
 (require 'term)
-
-(defun jest-ts--unquote (string)
-  (replace-regexp-in-string "[\"`']?\\(.*\\)[\"`']$" "\\1" string))
-
-(defun jest-ts--get-typescript-parser ()
-  (let ((parser (tsc-make-parser)))
-    (tsc-set-language parser (tree-sitter-require 'typescript))
-    parser))
 
 (defcustom jest-ts-query-sexp
   '((call_expression [(identifier) (member_expression)]
@@ -19,15 +31,29 @@
   :type 'sexp
   :group 'jest-ts)
 
+(defun jest-ts--create-parser (language)
+  "Create a new parser and set language to language"
+  (let ((parser (tsc-make-parser)))
+    (tsc-set-language parser (tree-sitter-require language))
+    parser))
+
+(defun jest-ts--parse-string (string query-sexp language)
+  "Parse string using query-sexp in language"
+  (let* ((parser (jest-ts--create-parser language))
+         (tree (tsc-parse-string parser string))
+         (root-node (tsc-root-node tree))
+         (query (tsc-make-query (tree-sitter-require language)
+                                query-sexp)))
+    (tsc-query-captures query root-node #'tsc--buffer-substring-no-properties)))
+
+(defun jest-ts--unquote (string)
+  "Removes quotes from beginning and end of string"
+  (replace-regexp-in-string "[\"`']?\\(.*?\\)[\"`']?$" "\\1" string))
+
 (defun jest-ts-tests ()
   "Get the jest tests defined in the current buffer"
-  (let* ((buffer-text (buffer-substring-no-properties (point-min) (point-max)))
-         (parser (jest-ts--get-typescript-parser))
-         (tree (tsc-parse-string parser buffer-text))
-         (root-node (tsc-root-node tree))
-         (query (tsc-make-query (tree-sitter-require 'typescript)
-                                jest-ts-query-sexp))
-         (query-result (tsc-query-captures query root-node #'ts--buffer-substring-no-properties))
+  (let* ((buffer-text (buffer-substring-no-properties (point-min) (point-max)))  
+         (query-result (jest-ts--parse-string buffer-text jest-ts-query-sexp 'typescript))
          (match-tuples (seq-partition (append query-result nil) 3)))
     (mapcar (lambda (test)
               (list (tsc-node-position-range (alist-get 'contents test))
